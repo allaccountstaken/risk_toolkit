1c5e0ff1f0baef4d98b946bf53d14422c39d7c1f
Dmitry Mikhaylov <dmik@vt.edu>
2020-06-09 16:11:25 -0400

    expend cir function

--- risk_kit.py
+++ risk_kit.py
@@ -2,7 +2,9 @@ import pandas as pd
 import numpy as np
 import scipy.stats as ss
 import scipy
-
+import pandas as pd
+import numpy as np
+import math
 
 
 
@@ -524,25 +526,106 @@ def gbm(n_years = 10, n_scenarios=1000, mu=0.07, sigma=0.15, steps_per_year=12, 
 
 
 
+# def discount(t, r):
+#     """Compute teh price of a pure disocunt bond that 
+#     pays 1$ at time t given interest rate r
+#     """
+#     return (1+r)**(-t)
+
+
+# def pv(l, r):
+#     """
+#     Computes PV of a sequence of liabilities
+#     l is indexed by the time and teh values are the amounts
+#     of each liability.
+#     Returns the present value
+#     """
+#     dates = l.index
+#     discounts = discount(dates, r)
+#     return (discounts * l).sum()
+
+
+# def funding_ratio(assets, liabilities, r):
+#     """Computes the funding ratio"""
+#     return assets / pv(liabilities, r)
+
+
 def discount(t, r):
-    """Compute teh price of a pure disocunt bond that 
-    pays 1$ at time t given interest rate r
     """
-    return (1+r)**(-t)
+    Compute the price of a pure discount bond that pays a dollar at time period t
+    and r is the per-period interest rate
+    returns a |t| x |r| Series or DataFrame
+    r can be a float, Series or DataFrame
+    returns a DataFrame indexed by t
+    """
+    discounts = pd.DataFrame([(r+1)**-i for i in t])
+    discounts.index = t
+    return discounts
 
-
-def pv(l, r):
+def pv(flows, r):
     """
-    Computes PV of a sequence of liabilities
-    l is indexed by the time and teh values are the amounts
-    of each liability.
-    Returns the present value
+    Compute the present value of a sequence of cash flows given by the time (as an index) and amounts
+    r can be a scalar, or a Series or DataFrame with the number of rows matching the num of rows in flows
     """
-    dates = l.index
+    dates = flows.index
     discounts = discount(dates, r)
-    return (discounts * l).sum()
-
+    return discounts.multiply(flows, axis='rows').sum()
 
 def funding_ratio(assets, liabilities, r):
-    """Computes the funding ratio"""
-    return assets / pv(liabilities, r)
+    """
+    Computes the funding ratio of a series of liabilities, based on an interest rate and current value of assets
+    """
+    return pv(assets, r)/pv(liabilities, r)
+
+def inst_to_ann(r):
+    """
+    Convert an instantaneous interest rate to an annual interest rate
+    """
+    return np.expm1(r)
+
+def ann_to_inst(r):
+    """
+    Convert an instantaneous interest rate to an annual interest rate
+    """
+    return np.log1p(r)
+
+def cir(n_years = 10, n_scenarios=1, a=0.05, b=0.03, sigma=0.05, steps_per_year=12, r_0=None):
+    """
+    Generate random interest rate evolution over time using the CIR model
+    b and r_0 are assumed to be the annualized rates, not the short rate
+    and the returned values are the annualized rates as well
+    """
+    if r_0 is None: r_0 = b 
+    r_0 = ann_to_inst(r_0)
+    dt = 1/steps_per_year
+    num_steps = int(n_years*steps_per_year) + 1 # because n_years might be a float
+    
+    shock = np.random.normal(0, scale=np.sqrt(dt), size=(num_steps, n_scenarios))
+    rates = np.empty_like(shock)
+    rates[0] = r_0
+
+    ## For Price Generation
+    h = math.sqrt(a**2 + 2*sigma**2)
+    prices = np.empty_like(shock)
+    ####
+
+    def price(ttm, r):
+        _A = ((2*h*math.exp((h+a)*ttm/2))/(2*h+(h+a)*(math.exp(h*ttm)-1)))**(2*a*b/sigma**2)
+        _B = (2*(math.exp(h*ttm)-1))/(2*h + (h+a)*(math.exp(h*ttm)-1))
+        _P = _A*np.exp(-_B*r)
+        return _P
+    prices[0] = price(n_years, r_0)
+    ####
+    
+    for step in range(1, num_steps):
+        r_t = rates[step-1]
+        d_r_t = a*(b-r_t)*dt + sigma*np.sqrt(r_t)*shock[step]
+        rates[step] = abs(r_t + d_r_t)
+        # generate prices at time t as well ...
+        prices[step] = price(n_years-step*dt, rates[step])
+
+    rates = pd.DataFrame(data=inst_to_ann(rates), index=range(num_steps))
+    ### for prices
+    prices = pd.DataFrame(data=prices, index=range(num_steps))
+    ###
+    return rates, prices
